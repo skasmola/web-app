95f4f7804e811de93c76b0c52afafb638755a18f
skasmola <skasmola@asu.edu>
2024-10-03 13:44:16 -0700

    Navbar with icons

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 import { NAV_LINKS } from "@/constants";
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import Person from "@mui/icons-material/Person";
 
 export default function Navbar() {
+
+  const [hoveredKey, setHoveredKey] = useState('');
+
   return (
     <nav className="flex justify-between max-container padding-container relative z-30 p-5 shadow-md">
       <Link href="/">
@@ -10,14 +16,46 @@ export default function Navbar() {
       </Link>
       <ul className="hidden h-full gap-12 lg:flex">
         {NAV_LINKS.map((link) => (
-          <Link
+          <div className="relative h-fit w-fit" key={link.key} onMouseEnter={() => setHoveredKey(link.key)} onMouseLeave={() => setHoveredKey('')}>
+            <Link
             href={link.href}
             key={link.key}
-            className="text-black text-lg font-extralight cursor-pointer transition-all hover:text-[#265B9D] hover:font-bold flex items-center justify-center mt-1"
-            style={{}}
+            className="text-black text-lg font-extralight cursor-pointer transition-all hover:font-semibold flex items-center justify-center mt-1"
           >
             {link.label}
-          </Link>
+            <span 
+              style={{ 
+                transform: hoveredKey === link.key ? "scaleX(1)" : "scaleX(0)"
+              }}
+            className="absolute -bottom-2 -left-2 -right-2 h-1 
+              origin-left rounded-full bg-[#265B9D] transition-transform duration-300 ease-out" />
+            <AnimatePresence>
+            {hoveredKey === link.key && link.dropdown && (
+              <motion.div 
+              initial={{ opacity: 0, y : 15}}
+              animate={{ opacity: 1, y: 0}}
+              exit={{ opacity: 0, y: 15}}
+              style={{ x : "-50%" }}
+              transition={{ duration: 0.3, ease: 'easeOut' }}
+              className="absolute left-1/2 top-12 bg-white text-black w-50">
+                <div className="absolute -top-6 left-0 right-0 h-auto bg-transparent" />
+                <div className="bg-white p-6 shadow-xl">
+                  {link.dropdown.map((item, index) => (
+                    <li className="py-2 flex items-center">
+                      {item.icon && (
+                        <item.icon className="mr-2 text-black" />
+                      )}
+                      <Link href='#' key={index} className="text-black text-lg font-extralight cursor-pointer transition-all hover:font-semibold whitespace-nowrap">
+                        {item.label}
+                      </Link>
+                    </li>
+                  ))}
+                </div> 
+              </motion.div>
+          )}
+          </AnimatePresence>
+            </Link>
+          </div>
         ))}
       </ul>
       <div className="relative hidden lg:flex">
@@ -38,7 +76,7 @@ export default function Navbar() {
         <input
           type="search"
           placeholder="Search..."
-          className="border-2 border-gray-300 pl-10 pr-4 py-2 text-gray-700 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="border-2 border-gray-300 pl-10 pr-4 py-2 text-gray-700 rounded-full focus:outline-none"
         />
       </div>
       <div className="hidden lg:flex items-center justify-center">
--- constants/index.ts
+++ constants/index.ts
@@ -1,7 +1,65 @@
-export const NAV_LINKS = [
-  { href: "/", key: "aboutus", label: "About us" },
-  { href: "/", key: "about", label: "Services" },
-  { href: "/", key: "contact", label: "Clients" },
+import AdminPanelSettingsRoundedIcon from '@mui/icons-material/AdminPanelSettingsRounded';
+import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
+import BarChartRoundedIcon from '@mui/icons-material/BarChartRounded';
+import ComputerRoundedIcon from '@mui/icons-material/ComputerRounded';
+import InsightsIcon from '@mui/icons-material/Insights';
+import Inventory2RoundedIcon from '@mui/icons-material/Inventory2Rounded';
+import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
+import MiscellaneousServicesIcon from '@mui/icons-material/MiscellaneousServices';
+import NewspaperIcon from '@mui/icons-material/Newspaper';
+import PeopleAltRoundedIcon from '@mui/icons-material/PeopleAltRounded';
+import PersonIcon from '@mui/icons-material/Person';
+import PublishedWithChangesRoundedIcon from '@mui/icons-material/PublishedWithChangesRounded';
+import StorageRoundedIcon from '@mui/icons-material/StorageRounded';
+import { SvgIconTypeMap } from '@mui/material';
+import { OverridableComponent } from '@mui/material/OverridableComponent';
+import SupportAgentIcon from '@mui/icons-material/SupportAgent';
+
+type DropDownItem = {
+  href: string,
+  key: string,
+  label: string,
+  icon?: OverridableComponent<SvgIconTypeMap<{}, "svg">> & {
+    muiName: string;
+  },
+}
+
+type NavLink = {
+  href: string;
+  key: string;
+  label: string;
+  dropdown?: DropDownItem[];
+};
+
+export const NAV_LINKS: NavLink[] = [
+  { href: "/", key: "aboutus", label: "About us",
+    dropdown: [
+      { href: "/leadership", key: "leadership", label: "Leadership", icon: PersonIcon  },
+      { href: "/insights", key: "insights", label: "Insights", icon: InsightsIcon },
+      { href: "/news", key: "news", label: "News", icon: NewspaperIcon }
+    ]
+   },
+  { href: "/", key: "services", label: "Services", 
+    dropdown: [
+    { href: "/health-it", key: "healthit", label: "Health IT", icon: LocalHospitalIcon },
+    { href: "/digital-services", key: "digitalservices", label: "Digital Services", icon: SupportAgentIcon },
+    { href: "/web-development", key: "webdevelopment", label: "Web Development", icon: ComputerRoundedIcon },
+    { href: "/data-management-analytics", key: "datamanagementanalytics", label: "Data Management and Analytics", icon: BarChartRoundedIcon },
+    { href: "/ai-and-machine-learning", key: "aiandmachinelearning", label: "AI and Machine Learning", icon: AutoAwesomeIcon },
+    { href: "/it-application-services", key: "itapplicationservices", label: "IT Application Services", icon: MiscellaneousServicesIcon },
+    { href: "/database-management", key: "databasemanagement", label: "Database Management", icon: StorageRoundedIcon},
+    { href: "/information-security", key: "informationsecurity", label: "Information Security", icon: AdminPanelSettingsRoundedIcon },
+    { href: "/quality-assurance-and-testing", key: "qualityassuranceandtesting", label: "Quality Assurance and Testing", icon: PublishedWithChangesRoundedIcon },
+    { href: "/staff-augmentation", key: "staffaugmentation", label: "Staff Augmentation", icon: PeopleAltRoundedIcon},
+    { href: "/products", key: "products", label: "Products", icon: Inventory2RoundedIcon },
+  ] 
+},
+  { href: "/", key: "clients", label: "Clients" },
   { href: "/", key: "vehicles", label: "Vehicles" },
-  { href: "/", key: "careers", label: "Careers" },
+  { href: "/", key: "careers", label: "Careers", 
+    dropdown: [
+      { href: "/careers/internship-program", key: "internshipprogram", label: "Internship Program"},
+      { href: "/careers/current-openings", key: "currentopenings", label: "Current Openings"}
+    ]
+   },
 ];
